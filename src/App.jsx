4a0812d24504abc1871d1f8162051061b8446e4f
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-11-17 17:09:52 +0200

    added playbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import MainPage from "./pages/MainPage/MainPage";
 import style from "./global.module.scss";
+import MainPage from "./pages/MainPage/MainPage";
+import Playbar from "./components/Playbar/Playbar";
 
 const App = () => {
   return (
     <div className={style.wrapper}>
       <MainPage />
+      <Playbar />
     </div>
   );
 };
